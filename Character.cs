c7fcb2779ca58bef5664f8f7329af40da1461f79
PrincessaKosatka <bender1295@gmail.com>
2026-09-08 15:48:31 +0300

    Add Character class with constructor validation

---
+++ Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GamePrototype
+{
+    internal class Character
+    {
+        public string Name { get; }
+        public int Health { get; private set; }
+        public int MaxHealth { get; }
+        public int BaseDamage { get; }
+
+        public Character(string name, int maxHealth, int baseDamage)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Имя персонажа не может быть пустым", nameof(name));
+            }
+
+            if (maxHealth <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxHealth), "Максимальное здоровье должно быть больше нуля");
+            }
+
+            if (baseDamage < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(baseDamage), "базовый урон не может быть отрицательным");
+            }
+
+            Name = name;
+            MaxHealth = maxHealth;
+            Health = maxHealth;
+            BaseDamage = baseDamage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace GamePrototype
+﻿using System;
+
+namespace GamePrototype
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Character player = new Character("Arthur", 30, 6);
+
+            Character goblin = new Character("Goblin", 18, 2);
+
+            Console.WriteLine($"{player.Name}: {player.Health}/{player.MaxHealth} HP, урон: {player.BaseDamage}");
+
+            Console.WriteLine($"{goblin.Name}: {goblin.Health}/{goblin.MaxHealth} HP, урон {goblin.BaseDamage}");
         }
     }
+
 }
